93bdb3b3eec1e9965d13aac8cc1a4be4058008d5
Kenny Hollaway <hollawaykenny007@gmail.com>
2020-09-17 22:50:00 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,6 +2,8 @@ def my_collect(array)
   array = ["Tim Jones", "Tom Smith", "Jim Camp"]
   my_collect(array) do |name|
     name.split(" ").first 
+  collection = ['ruby', 'javascript', 'python', 'objective-c']
+  my_collect(collection) do |lang|
+    lang.upcase
   end
-  
 end
